bc105bce6e569e926eb9d61673d6c91dbf382a47
Aleksandr Barmin <abarmin@outlook.com>
2022-03-18 15:04:14 +0000

    All work in a reactive way

--- frontend/src/app/service/data.service.ts
+++ frontend/src/app/service/data.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@angular/core';
 import { of } from 'rxjs';
-import { zip } from 'rxjs';
 import { combineLatest } from 'rxjs';
 import { Subject } from 'rxjs';
-import { BehaviorSubject } from 'rxjs';
+import { filter } from 'rxjs';
 import { mapTo } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/internal/operators/map';
 import { DataRecord } from '../model/data-record';
-import { Device } from '../model/device';
 import { Flat } from '../model/flat';
 import { LocalStorageService } from './local-storage.service';
 
@@ -16,22 +15,33 @@ import { LocalStorageService } from './local-storage.service';
   providedIn: 'root'
 })
 export class DataService extends LocalStorageService<DataRecord> {
+  private records: Subject<DataRecord[]> = new BehaviorSubject<DataRecord[]>([]);
+  public readonly records$ = this.records.asObservable();
+
+  constructor() {
+    super();
+    const items = this.getStorage('records');
+    this.records.next(items);
+  }
+
   public save(record: DataRecord): Observable<DataRecord> {
     const items = this.getStorage('records');
     items.push(record);
     this.setStorage('records', items);
 
+    this.records.next(items);
+
     return of(record);
   }
 
   public findRecordsForFlat(flat$: Observable<Flat>): Observable<DataRecord[]> {
-    return flat$.pipe(
-      map(flat => {
-        const items = this.getStorage('records');
-        const filtered = items.filter(item => item.flat.alias == flat.alias);
-        return filtered;
-      })
-    )
+    return combineLatest([this.records$, flat$])
+      .pipe(
+        map(([records, flat]) => {
+          const filtered = records.filter(record => record.flat.alias == flat.alias)
+          return filtered;
+        })
+      );
   }
 
   public findRecordsForFlatAndYear(
@@ -39,10 +49,9 @@ export class DataService extends LocalStorageService<DataRecord> {
     year$: Observable<Number>
     ): Observable<DataRecord[]> {
 
-    return combineLatest([flat$, year$]).pipe(
-      map(([flat, year]) => {
-        const items = this.getStorage('records');
-        const filtered = items
+    return combineLatest([this.records$, flat$, year$]).pipe(
+      map(([records, flat, year]) => {
+        const filtered = records
           .filter(item => {
             return item.flat.alias = flat.alias
           })
