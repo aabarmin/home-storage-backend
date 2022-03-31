3d617b72bb3eda35de3205758ea218d8ffd1e09b
Aleksandr Barmin <abarmin@outlook.com>
2022-03-31 11:47:21 +0100

    Information about version is externalized

---
+++ frontend/src/app/model/version-info.ts
@@ -0,0 +1,5 @@
+export interface VersionInfo {
+  name: string;
+  version: string;
+  build: string;
+}
--- frontend/src/app/toolbar/toolbar.component.ts
+++ frontend/src/app/toolbar/toolbar.component.ts
@@ -1,16 +1,32 @@
 import { Output, EventEmitter } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { VersionInfo } from '../model/version-info';
+import { VersionService } from '../service/version.service';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.css']
+  styleUrls: ['./toolbar.component.css'],
 })
-export class ToolbarComponent {
+export class ToolbarComponent implements OnInit {
   @Output()
   onSidebarToggle: EventEmitter<any> = new EventEmitter<any>();
 
+  versionInfo: VersionInfo = {
+    version: 'not set',
+    build: 'not set',
+    name: 'not set',
+  };
+
+  constructor(private versionService: VersionService) {}
+
   public sidebarToggleClick() {
     this.onSidebarToggle.emit();
+  }
+
+  ngOnInit(): void {
+    this.versionService.getEnvironmentInfo().subscribe((version) => {
+      this.versionInfo = version;
+    });
   }
 }
