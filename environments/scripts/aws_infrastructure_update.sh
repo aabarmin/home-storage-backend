06a79c227679767ff303caeca34c3214b0476988
Aleksandr Barmin <abarmin@outlook.com>
2022-04-12 18:41:08 +0100

    Moving configuration to AWS SSM Parameter store
    Closes #35

--- environments/scripts/aws_infrastructure_update.sh
+++ environments/scripts/aws_infrastructure_update.sh
@@ -11,7 +11,26 @@ else
 fi
 
 TEMPLAET_BODY_PARAM="file://${TEMPLATE_PATH}"
+STACK_NAME="abarmin-home-storage"
 
 aws cloudformation update-stack \
-    --stack-name abarmin-home-storage \
+    --stack-name ${STACK_NAME} \
     --template-body ${TEMPLAET_BODY_PARAM}
+
+echo "Waiting till stack is updated"
+
+aws cloudformation wait stack-update-complete \
+    --stack-name ${STACK_NAME}
+
+echo "Updating Client Secret"
+
+USER_POOL_PARAMETER="/prod/home-storage/cognito/pool-id"
+USER_POOL_ID=$(aws ssm get-parameter --name ${USER_POOL_PARAMETER} | jq -r '.Parameter.Value')
+
+CLIENT_PARAMETER="/prod/home-storage/cognito/client-id"
+CLIENT_ID=$(aws ssm get-parameter --name ${CLIENT_PARAMETER} | jq -r '.Parameter.Value')
+
+CLIENT_SECRET=$(aws cognito-idp describe-user-pool-client --user-pool-id ${USER_POOL_ID} --client-id ${CLIENT_ID} | jq -r '.UserPoolClient.ClientSecret')
+
+SECRET_PARAMETER="/prod/home-storage/cognito/client-secret"
+aws ssm put-parameter --name ${SECRET_PARAMETER} --value ${CLIENT_SECRET} --type String --overwrite
