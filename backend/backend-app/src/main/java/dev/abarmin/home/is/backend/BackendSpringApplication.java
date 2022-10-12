ada9023a0e193e9a30c5d3c39611afc33a2a0861
Aleksandr Barmin <barmin.alexander@gmail.com>
2022-10-12 12:22:50 +0100

    #79 Getting rid of SSM Parameter store (#80)

--- backend/backend-app/src/main/java/dev/abarmin/home/is/backend/BackendSpringApplication.java
+++ backend/backend-app/src/main/java/dev/abarmin/home/is/backend/BackendSpringApplication.java
@@ -1,10 +1,7 @@
 package dev.abarmin.home.is.backend;
 
-import dev.abarmin.home.is.backend.ssm.parameter.SsmParameterStoreApplicationContextInitializer;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 /**
  * @author Aleksandr Barmin
@@ -13,9 +10,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 public class BackendSpringApplication {
   public static void main(String[] args) {
     new SpringApplicationBuilder(BackendSpringApplication.class)
-        .initializers(
-            new SsmParameterStoreApplicationContextInitializer()
-        )
         .build()
         .run(args);
   }
